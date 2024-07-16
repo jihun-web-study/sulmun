51627eb189ff4d98fa567c30a5cf49e1055dfef0
JIHU96 <korjihu@gmail.com>
2024-07-16 21:59:12 +0900

    chore: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "public/": `${path.resolve(__dirname, "public")}/`,
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
+
+/* 
+"public/": `${path.resolve(__dirname, "public")}/`,
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+*/
