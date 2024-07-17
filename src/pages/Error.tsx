965193aa13aedbbeebb0710533ca7cbe890182f1
JIHU96 <korjihu@gmail.com>
2024-07-17 17:08:57 +0900

    feat: 없는 라우트에 대한 에러 페이지 생성

---
+++ src/pages/Error.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-400 bg-opacity-50 flex justify-center items-center">
+      <div className="w-2/4 h-2/4 flex flex-col justify-center items-center bg-green-500">
+        <div>404 Error!!!</div>
+        <Link to={"/"}> 홈으로 돌아가기</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Error;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { memo } from "react";
-import HomePage from "../pages/HomePage";
-import Sample from "../pages/Sample";
+import HomePage from "@/pages/HomePage";
+import Sample from "@/pages/Sample";
+import Error from "@/pages/Error";
 
 const Router = (): JSX.Element => {
   return (
     <Routes>
-      {/* <Route path="*" element={<Error />} /> */}
+      <Route path="*" element={<Error />} />
       <Route path="/" element={<HomePage />} />
       <Route path="/sample" element={<Sample />} />
     </Routes>
