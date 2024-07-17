d196a71b47e4aea923ad87ec640a78fcce1ccbc4
JIHU96 <korjihu@gmail.com>
2024-07-17 17:08:14 +0900

    design: tailwind.config에 스크롤바 표시 클래스 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,34 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".scrollbar-hide": {
+          /* IE and Edge */
+          "-ms-overflow-style": "none",
+          /* Firefox */
+          "scrollbar-width": "none",
+          /* Safari and Chrome */
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        },
+        ".scrollbar-default": {
+          /* IE and Edge */
+          "-ms-overflow-style": "auto",
+          /* Firefox */
+          "scrollbar-width": "auto",
+          /* Safari and Chrome */
+          "&::-webkit-scrollbar": {
+            display: "block",
+          },
+        },
+        ".children-shrink-0": {
+          "& > * ": { "flex-shrink": 0 },
+        },
+      };
+      addUtilities(newUtilities, ["responsive"]);
+    },
+  ],
 };
