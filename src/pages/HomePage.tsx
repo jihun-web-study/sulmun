52a43a8e157ed976bb5ce6fb5f1826ca52273c3c
JIHU96 <korjihu@gmail.com>
2024-07-22 12:45:01 +0900

    temp

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,25 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "../../public/vite.svg";
-import "../App.css";
+import { useAtomValue } from "jotai";
+import { countryAtom } from "@/store/sample";
 
 const HomePage = () => {
-  const [count, setCount] = useState(0);
+  const country = useAtomValue(countryAtom);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col gap-2">
+      <div>{`county: ${country}`}</div>
+      <div>Main Content</div>
+      <div className="h-[100px] bg-yellow-200">'hi'</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">hi</div>
+      <div className="h-[100px] bg-yellow-200">End</div>
+    </div>
   );
 };
 
