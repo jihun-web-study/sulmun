7606f40e679e8b858092c1b73cd380c84124e4c0
JIHU96 <korjihu@gmail.com>
2024-07-23 19:30:03 +0900

    design: 로그인/회원가입 페이지 UI 구현

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,5 +1,47 @@
+import Input from "@/components/auth/common/Input";
+import SocialLoginbutton from "@/components/auth/signin/SocialLoginbutton";
+import googleLogo from "@/assets/svg/google.svg";
+import kakaoLogo from "@/assets/svg/kakao.svg";
+import { Link } from "react-router-dom";
+
 const SignInPage = () => {
-  return <div>SignInPage</div>;
+  return (
+    <>
+      <form
+        className="w-full flex flex-col gap-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("submit!");
+        }}
+      >
+        <div className="flex gap-4">
+          <div className="flex-grow flex flex-col gap-2">
+            <Input type="text" placeholder="아이디" />
+            <Input type="password" placeholder="비밀번호" />
+          </div>
+          <button className="w-24 text-white rounded bg-proj-bg-linear">
+            로그인
+          </button>
+        </div>
+      </form>
+      <div className="my-4">
+        <span>계정이 없으신가요?</span>
+        <Link
+          to={"/auth/signup"}
+          className="ml-[16px] text-gray-500 font-bold leading-8 underline decoration-[2px] underline-offset-[4px]"
+        >
+          회원가입
+        </Link>
+      </div>
+      <div className="flex flex-col items-center">
+        <strong className="mb-4">소셜 계정으로 로그인하기</strong>
+        <div className="flex gap-3">
+          <SocialLoginbutton to={"/"} img={kakaoLogo} alt={"kakao"} />
+          <SocialLoginbutton to={"/"} img={googleLogo} alt={"google"} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignInPage;
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,5 +1,41 @@
+import Input from "@/components/auth/common/Input";
+import CheckButton from "@/components/auth/signup/CheckButton";
+import SocialLoginbutton from "@/components/auth/signin/SocialLoginbutton";
+import googleLogo from "@/assets/svg/google.svg";
+import kakaoLogo from "@/assets/svg/kakao.svg";
+import { Link } from "react-router-dom";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <form
+      className="w-full flex flex-col gap-3"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log("submit!");
+      }}
+    >
+      <div className=" flex flex-grow gap-4">
+        <Input type="text" placeholder="이메일" />
+        <CheckButton onClick={() => console.log("인증번호 전송")}>
+          인증번호 전송
+        </CheckButton>
+      </div>
+      <div className=" flex flex-grow gap-4">
+        <Input type="text" placeholder="인증번호" />
+        <CheckButton onClick={() => console.log("인증번호 확인")}>
+          인증번호 확인
+        </CheckButton>
+      </div>
+      <Input type="password" placeholder="비밀번호" />
+      <Input type="password" placeholder="비밀번호 확인" />
+      <button
+        type="submit"
+        className="w-full py-5 text-white rounded-[10px] bg-proj-bg-linear"
+      >
+        회원가입하기
+      </button>
+    </form>
+  );
 };
 
 export default SignUpPage;
