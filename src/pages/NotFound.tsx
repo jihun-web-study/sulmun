d62c4afb965c037016788f58ede978b0237c388b
JIHU96 <korjihu@gmail.com>
2024-07-24 01:05:27 +0900

    design: 404 에러 페이지 색상 변경

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-gray-400 bg-opacity-50 flex justify-center items-center">
-      <div className="w-2/4 h-2/4 flex flex-col justify-center items-center bg-green-500">
-        <div>404 Error!!!</div>
-        <Link to={"/"}> 홈으로 돌아가기</Link>
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-300 bg-opacity-50 flex justify-center items-center">
+      <div className="w-2/4 aspect-square text-white flex flex-col justify-center items-center bg-proj-color rounded-2xl">
+        <div className="text-4xl ">404 Error!!!</div>
+        <Link
+          to={"/"}
+          className="text-3xl text-white hover:text-black underline decoration-[1px] underline-offset-[4px]"
+        >
+          {" "}
+          홈으로 돌아가기
+        </Link>
       </div>
     </div>
   );
