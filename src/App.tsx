fba2347457272299a58dcf0a1d2a3147163beba7
JIHU96 <korjihu@gmail.com>
2024-07-23 19:07:50 +0900

    fix: 루트 레이아웃을 라우트에 적용하느 Outlet으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/router";
-import FlexLayout from "@/components/layout/flexLayout/FlexLayout";
-import GridLayout from "@/components/layout/gridLayout/GridLayout";
-
-//import "./test.css";
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
-      {/* <FlexLayout>
-        <Router />
-      </FlexLayout> */}
-
-      {/*  */}
-
-      <GridLayout>
-        <Router />
-      </GridLayout>
+      <Router />
     </BrowserRouter>
   );
 };
