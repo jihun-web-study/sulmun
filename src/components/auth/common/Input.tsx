db3147006f6e0673cf6d7b836adc1ff48e33eab2
JIHU96 <korjihu@gmail.com>
2024-07-24 21:06:45 +0900

    feat: Auth 공용 컴포넌트 Input의 props인 value의 기본값과 onChange 적용

--- src/components/auth/common/Input.tsx
+++ src/components/auth/common/Input.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import { Dispatch, SetStateAction } from "react";
 
 type InputProps = {
   type: "text" | "password";
   placeholder: string;
   value?: string;
+  onChange: Dispatch<SetStateAction<string>>;
 };
 
-const Input = ({ type = "text", placeholder, value }: InputProps) => {
+const Input = ({ type = "text", onChange, placeholder, value }: InputProps) => {
   return (
     <input
       className="w-full h-[50px] border-solid border-[1px] rounded-[10px] pl-[16px]"
       type={type}
       placeholder={placeholder}
-      value={value}
+      value={value || ""}
+      onChange={(e) => onChange(e.target.value)}
     />
   );
 };
