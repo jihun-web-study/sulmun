77218ce583cdef659ea1f6c0399cb1686f40d69c
JIHU96 <korjihu@gmail.com>
2024-07-25 00:11:51 +0900

    feat: Supabase 이메일 로그인, 깃허브 소셜로그인 기능 구현

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import Input from "@/components/auth/common/Input";
 import SocialLoginbutton from "@/components/auth/signin/SocialLoginbutton";
-import googleLogo from "@/assets/svg/google.svg";
-import kakaoLogo from "@/assets/svg/kakao.svg";
-import { Link } from "react-router-dom";
+import { signInWithEmail, signInWithGithub } from "@/supabase/utils";
 
 const SignInPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <form
@@ -12,18 +15,30 @@ const SignInPage = () => {
         onSubmit={(e) => {
           e.preventDefault();
           console.log("submit!");
+          signInWithEmail({ email, password });
         }}
       >
         <div className="flex gap-4">
           <div className="flex-grow flex flex-col gap-2">
-            <Input type="text" placeholder="아이디" />
-            <Input type="password" placeholder="비밀번호" />
+            <Input
+              type="text"
+              placeholder="아이디"
+              value={email}
+              onChange={setEmail}
+            />
+            <Input
+              type="password"
+              placeholder="비밀번호"
+              value={password}
+              onChange={setPassword}
+            />
           </div>
           <button className="w-24 text-white rounded bg-proj-bg-linear">
             로그인
           </button>
         </div>
       </form>
+
       <div className="my-4">
         <span>계정이 없으신가요?</span>
         <Link
@@ -36,8 +51,15 @@ const SignInPage = () => {
       <div className="flex flex-col items-center">
         <strong className="mb-4">소셜 계정으로 로그인하기</strong>
         <div className="flex gap-3">
-          <SocialLoginbutton to={"/"} img={kakaoLogo} alt={"kakao"} />
-          <SocialLoginbutton to={"/"} img={googleLogo} alt={"google"} />
+          <SocialLoginbutton
+            socialType={"kakao"}
+            onClick={() => alert("구글 로그인은 아직 미구현입니다.")}
+          />
+          <SocialLoginbutton
+            socialType={"google"}
+            onClick={() => alert("구글 로그인은 아직 미구현입니다.")}
+          />
+          <SocialLoginbutton socialType={"github"} onClick={signInWithGithub} />
         </div>
       </div>
     </>
