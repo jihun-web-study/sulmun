366811cee9bb7f640bc53ee3979478da858d730a
JIHU96 <korjihu@gmail.com>
2024-07-29 19:56:28 +0900

    feat: 헤더 아래 영역들에 상단 패딩 추가

--- src/components/layout/gridLayout/AsideGrid.tsx
+++ src/components/layout/gridLayout/AsideGrid.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 const AsideGrid = () => {
   return (
-    <aside className="w-full xl:pr-[calc(100%/6)]">
+    <aside className="pt-6 w-full xl:pr-[calc(100%/6)] bg-[#EEEEEE]">
       <div className="bg-red-200 w-full h-full">Aside</div>
     </aside>
   );
