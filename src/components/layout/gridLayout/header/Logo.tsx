6a1da6b81e02e94025cfa8935f70e5a5b395073c
JIHU96 <korjihu@gmail.com>
2024-07-29 19:29:57 +0900

    feat: 헤더-로고에 텍스트 테두리 추가

--- src/components/layout/gridLayout/header/Logo.tsx
+++ src/components/layout/gridLayout/header/Logo.tsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <h1 className="w-full h-full xl:pl-[60px] flex items-center">
-      <Link to={"/"} className="text-proj-color font-bold text-3xl pl-2">
+      <Link
+        to={"/"}
+        className="text-shadow text-proj-color font-bold text-3xl pl-2"
+      >
         SULMUN
       </Link>
     </h1>
