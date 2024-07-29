266e990617414a323eee2ce2c9c9de0d0e38aa7e
JIHU96 <korjihu@gmail.com>
2024-07-29 19:09:07 +0900

    feat: 헤더-검색바 UI 구현

--- src/components/layout/gridLayout/header/SearchBar.tsx
+++ src/components/layout/gridLayout/header/SearchBar.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { FormEvent, useState } from "react";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [value, setValue] = useState("");
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit ", value);
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="w-full h-full flex items-center">
+      <input
+        type="text"
+        value={value}
+        placeholder="검색 키워드"
+        onChange={(e) => setValue(e.target.value)}
+        className="w-full h-1/2 px-3 placeholder:text-center rounded-lg text-[#999999] bg-[#F2F2F2]"
+      />
+    </form>
+  );
 };
 
 export default SearchBar;
