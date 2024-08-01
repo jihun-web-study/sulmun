68d30af37d17c9fa6d92c3061683dbe808ad85ff
JIHU96 <korjihu@gmail.com>
2024-08-01 17:36:34 +0900

    design: 404 페이지에 gap 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-gray-300 bg-opacity-50 flex justify-center items-center">
-      <div className="w-2/4 aspect-square text-white flex flex-col justify-center items-center bg-proj-color rounded-2xl">
+      <div className="w-2/4 aspect-square text-white flex flex-col justify-center items-center gap-4 bg-proj-color rounded-2xl">
         <div className="text-4xl ">404 Error!!!</div>
         <Link
           to={"/"}
