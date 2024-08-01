7df85c11c6760164798c3fcc9c732ef0c7428ebb
JIHU96 <korjihu@gmail.com>
2024-08-01 21:27:09 +0900

    fix: main 요소의 하단에도 padding 추가

--- src/components/layout/gridLayout/GridLayout.tsx
+++ src/components/layout/gridLayout/GridLayout.tsx
@@ -9,7 +9,7 @@ const GridLayout = () => {
     <div className="grid grid-cols-[1fr_3fr_1fr] xl:grid-cols-[360px_3fr_360px] grid-rows-[auto_1fr] w-screen h-screen overflow-hidden">
       <HeaderGrid />
       <NavGrid />
-      <main className="pt-6 bg-[#EEEEEE] lg:pl-0 w-full h-full flex flex-col w-full justify-self-center overflow-y-auto scrollbar-hide children-shrink-0">
+      <main className="py-6 bg-[#EEEEEE] lg:pl-0 w-full h-full flex flex-col w-full justify-self-center overflow-y-auto scrollbar-hide children-shrink-0">
         <Outlet />
       </main>
       <AsideGrid />
