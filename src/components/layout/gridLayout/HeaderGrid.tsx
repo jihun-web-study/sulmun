fc86753575b6e89a96207a7b4ebc2357b1a3e151
JIHU96 <korjihu@gmail.com>
2024-08-08 17:09:03 +0900

    design: Header 컴포넌트에 가독성을 위해 하단 테두리 추가

--- src/components/layout/gridLayout/HeaderGrid.tsx
+++ src/components/layout/gridLayout/HeaderGrid.tsx
@@ -4,7 +4,7 @@ import UserInfo from "@/components/layout/gridLayout/header/UserInfo";
 
 const HeaderGrid = () => {
   return (
-    <header className="col-span-3 w-full h-[80px] grid grid-cols-[1fr_3fr_1fr] xl:grid-cols-[360px_3fr_360px] items-center">
+    <header className="col-span-3 w-full h-[80px] grid grid-cols-[1fr_3fr_1fr] xl:grid-cols-[360px_3fr_360px] items-center border-b-2 border-[#EEEEEE]">
       <Logo />
       <SearchBar />
       <UserInfo />
